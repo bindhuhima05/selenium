0080d7da15c6da73a7df9d027b510bdbebe70855
himabindhu velakacharla <bindhuhima051@gmail.com>
2025-12-03 15:17:43 +0530

    First commit

---
+++ src/test/java/github/GithubPracticeTest.java
@@ -0,0 +1,16 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GithubPracticeTest {
+
+	@Test
+	public void githubPracticeTest() {
+		Reporter.log("Hi github", true);
+//		String BROWSER = System.getProperty("browser");
+//		System.out.println(BROWSER);
+//		String USERNAME = System.getProperty("username");
+//		System.out.println(USERNAME);
+	}
+}
